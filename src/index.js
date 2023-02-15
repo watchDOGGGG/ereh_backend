4ad0999cdad99cc3cb4e208a7fecdef61642fd1b
watchdogggg <odurandyjnr@gmail.com>
2023-02-15 11:11:58 +0100

    added cors origin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { Db_connection } from './db/index.js'
 import { Routes } from './routes/index.js'
+import cors from 'cors'
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -9,6 +10,7 @@ const app = express()
 Db_connection()
 
 app.use(express())
+app.use(cors())
 app.use(express.json())
 app.use('/files',express.static('./'))
 app.use('/v1/api',Routes)
