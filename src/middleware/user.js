331d049ceb31599255323f56dbae1bc2de327c78
watchdogggg <odurandyjnr@gmail.com>
2023-02-15 17:25:54 +0100

    Update user.js

--- src/middleware/user.js
+++ src/middleware/user.js
@@ -4,98 +4,111 @@ import { verify } from "jsonwebtoken"
 import { AccessToken } from "./helper/jwt.js"
 import { Topic } from "./feed.js"
 
-export class User{
+export class User {
 
-    static async checkUser(data){
-        const user = await userCollection.findOne({$or:[{_id:data},{username:data},{email:data}]})
-        if(!user){
-            return false
+    static async checkUser(type, data) {
+        if (type == "email") {
+            const user = await userCollection.findOne({ email: data })
+            if (!user) {
+                return false
+            }
+            return user
         }
-        return user
+
+        if (type == "id") {
+            const user = await userCollection.findOne({ _id: data })
+            if (!user) {
+                return false
+            }
+            return user
+        }
+
+
+
     }
 
-    static async getuser(req,res){
-        const request = await User.checkUser(req.params.user)
-        if(request === false){
-            return res.status(400).send({message: 'user not found'})
+    static async getuser(req, res) {
+        const request = await User.checkUser('id',req.params.user)
+        if (request === false) {
+            return res.status(400).send({ message: 'user not found' })
         }
-        return res.status(200).send({message:request})
+        return res.status(200).send({ message: request })
     }
 
-    static async RegisterUser (req, res) {
-        const {fullname, username, email, password} = req.body
+    static async RegisterUser(req, res) {
+        const { fullname, username, email, password } = req.body
 
         const salt = await bcrypt.genSalt(10)
         const new_password = await bcrypt.hash(password, salt)
 
-        const request = await User.checkUser(email)
+        const request = await User.checkUser('email',email)
 
-        if(request !== false){
-            return res.status(400).send({ message:'user with this email or username already exist'})
+        if (request !== false) {
+            return res.status(400).send({ message: 'user with this email or username already exist' })
         }
 
         const createuser = await userCollection.create({
             fullname: fullname,
-            email:email,
+            email: email,
             username: username,
             password: new_password
         })
 
-        if(!createuser){
-            return res.status(500).send({message:'error creating user'})
+        if (!createuser) {
+            return res.status(500).send({ message: 'error creating user' })
         }
-        return res.status(200).send({message:'user created successfully'})
+        return res.status(200).send({ message: 'user created successfully' })
     }
 
-    static async Login (req, res) {
-        const {email, password} = req.body
-        const request = await User.checkUser(email)
-        if(!request){
-            return res.status(404).send({message:"user not found"})
+    static async Login(req, res) {
+        const { email, password } = req.body
+        const request = await User.checkUser('email',email)
+        if (!request) {
+            return res.status(404).send({ message: "user not found" })
         }
         const verify_password = await bcrypt.compare(password, request.password)
-        if (!verify_password){
-            return res.status(401).send({message:"incorrect password"})
+        if (!verify_password) {
+            return res.status(401).send({ message: "incorrect password" })
         }
         const token = AccessToken.GenerateToken(request)
-        res.status(200).send({message:token,user:request})
+        res.status(200).send({ message: token, user: request })
         return
-        
+
     }
 
-    static async totalNumberofTopicperuser(req,res){
-        const request = await User.checkUser(req.params.user)
+    static async totalNumberofTopicperuser(req, res) {
+        const request = await User.checkUser('id',req.params.user)
         console.log(request)
-        if(request === false){
-            return res.status(404).send({message:'user not available'})
+        if (request === false) {
+            return res.status(404).send({ message: 'user not available' })
         }
-        const topicCount = await topicCollection.find({user:req.params.user}).countDocuments()
-        return res.status(200).send({message:topicCount})
+        const topicCount = await topicCollection.find({ user: req.params.user }).countDocuments()
+        return res.status(200).send({ message: topicCount })
     }
 
-    static async totalNumberofMembers(req,res){
+    static async totalNumberofMembers(req, res) {
         const members = await userCollection.find().countDocuments()
-        return res.status(200).send({message:members})
+        return res.status(200).send({ message: members })
     }
 
-    static async userProfile(req, res){
-        const request = await User.checkUser(req.params.user)
-        let topics; 
+    static async userProfile(req, res) {
+        const request = await User.checkUser('id',req.params.user)
+        let topics;
 
-        if(!request){
-            return res.status(404).send({message:'user not available'})
+        if (!request) {
+            return res.status(404).send({ message: 'user not available' })
         }
 
-        const fetchTopic = await topicCollection.find({user:req.params.user})
-        .populate({path:'user',select:''})
-        if(fetchTopic.length < 1){
+        const fetchTopic = await topicCollection.find({ user: req.params.user })
+            .populate({ path: 'user', select: '' })
+        if (fetchTopic.length < 1) {
             topics = "no topic yet"
-        }else{
+        } else {
             topics = fetchTopic
         }
 
-        return res.status(200).send({message:request, topics:topics})
+        return res.status(200).send({ message: request, topics: topics })
 
     }
-    
+
 }
